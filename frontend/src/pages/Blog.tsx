9be6b4371ce380aa2484c84f77e8b52f79a38667
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-04-18 22:09:34 +0530

    Initialised FE and added Routes

---
+++ frontend/src/pages/Blog.tsx
@@ -0,0 +1,12 @@
+const Blog = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex flex-col justify-center items-center">
+      <div className="max-w-md text-3xl font-bold">
+        "The customer service I received was exceptional. The support team went above and beyond to address my concerns."
+      </div>
+      
+    </div>
+    
+  )
+}
+export {Blog}
